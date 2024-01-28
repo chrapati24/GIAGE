88927202d9cfcc399c1a5ea797c4b9ab537d2d89
chrapati24 <des.abeilles.et.du.miel.24@gmail.com>
2024-01-28 17:00:23 +0100

    New user interraction : default-cube size

--- src/include/GlobalIncludes.h
+++ src/include/GlobalIncludes.h
@@ -7,5 +7,14 @@
 #include "raygui.h"
 #define _CRT_SECURE_NO_WARNINGS
 
+#if defined(PLATFORM_DESKTOP)
+	#define GLSL_VERSION            330
+#else   // PLATFORM_ANDROID, PLATFORM_WEB
+	#define GLSL_VERSION            100
+#endif
+
+#include "rlgl.h"
+
+
 Vector3 GIAGEVersion = { 1, 0, 0 };
 
